3c1e9f370830c01a74dfd91b9f28f032a9ad4401
Dennis Kerner <denkern@gmail.com>
2017-06-05 21:01:26 +0700

    init

---
+++ index.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const useragent= require('useragent')
+
+let server = http.createServer(function(req, res) {
+	console.log(JSON.stringify(req.headers));
+	res.writeHead(200, { 'Content-Type' : 'text/plain'});
+	res.write(JSON.stringify(req.headers));
+	
+	//console.log(req.socket.address());
+	res.write(JSON.stringify(req.socket.address()));
+
+	var agent = useragent.parse(req.headers['user-agent']);
+	res.write(JSON.stringify(agent));
+
+	res.end();
+});
+
+server.listen(8000);
